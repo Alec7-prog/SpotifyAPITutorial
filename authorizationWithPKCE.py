794275915d06d98f6d29718d9f5275e51fc51661
Alec7-prog <56138258+Alec7-prog@users.noreply.github.com>
2025-12-30 12:19:03 -0600

    finished with authorization with PKCE feature

--- authorizationWithPKCE.py
+++ authorizationWithPKCE.py
@@ -1,6 +1,18 @@
+from urllib.parse import urlencode, urlparse, urlunparse
+import os
+from dotenv import load_dotenv
 import secrets 
 import hashlib
 import base64
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+print(f"Client ID: {client_id}")
+redirect_uri = "http://127.0.0.1:3000"
+scope = 'user-read-private user-read-email'
+
+auth_base_url = "https://accounts.spotify.com/authorize"
 
 def generateCodeVerifier():
     possible = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~"
@@ -23,5 +35,20 @@ def base64url_encode(input_bytes):
     return encoded.rstrip(b'=').decode('utf-8')
 
 hashed = sha256(generateCodeVerifier())
+print(f"Hashed code verifier: {hashed}")
 codeChallenge = base64url_encode(hashed)
-print("Code Challenge:", codeChallenge)
+print(f"Code challenge: {codeChallenge}")
+
+params = {
+    'response_type':'code',
+    'client_id':client_id,
+    'scope':scope,
+    'code_challenge_method':'S256',
+    'code_challenge':codeChallenge,
+    'redirect_uri':redirect_uri
+}
+
+query_string = urlencode(params)
+full_auth_url = f"{auth_base_url}?{query_string}"
+
+print(f"Direct the user to: {full_auth_url}")
